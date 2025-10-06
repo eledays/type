6d8741e48ee81fe17e11f5a7472186cf9f40cd77
eledays <leotol1432@gmail.com>
2025-10-06 21:03:29 +0300

    style: word position in Clip

--- frontend/src/components/Clip/Clip.tsx
+++ frontend/src/components/Clip/Clip.tsx
@@ -1,4 +1,4 @@
-import WordCont from '../WordCont/WordCont'
+import WordCont from '../WordContainer/WordContainer'
 import styles from './Clip.module.css'
 import type { ClipProps } from '../../types/components'
 
@@ -6,7 +6,12 @@ export default function Clip({ word, index, style }: ClipProps) {
   return (
     <>
       <div className={styles.clip} style={style}>
-        <WordCont word={word} index={index} />
+        <div className={styles.wordContainer}>
+          <WordCont word={word} index={index} />
+        </div>
+        <div className={styles.answersContainer}>
+          {/* Answers would go here */}
+        </div>
       </div>
     </>
   )
