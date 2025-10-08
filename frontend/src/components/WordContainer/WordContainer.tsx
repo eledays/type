cc36b7831d81dba886f1e7b90cb5cfe44585053e
eledays <leotol1432@gmail.com>
2025-10-08 13:29:16 +0300

    style: word and answers

--- frontend/src/components/WordContainer/WordContainer.tsx
+++ frontend/src/components/WordContainer/WordContainer.tsx
@@ -1,9 +1,10 @@
 import MissingLetter from '../MissingLetter/MissingLetter'
 import type { WordContProps } from '../../types/components'
+import styles from './WordContainer.module.css'
 
 export default function WordCont({ word }: WordContProps) {
   return (
-    <div>
+    <div className={styles.wordContainer}>
       {word.text.slice(0, word.missingIndex)}
       <MissingLetter />
       {word.text.slice(word.missingIndex)}
