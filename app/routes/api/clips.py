a98e00c86c2320f82e892e4ac466688642f78179
eledays <leotol1432@gmail.com>
2025-10-28 14:01:34 +0300

    feat: sever html rendering for mock data

--- app/routes/api/clips.py
+++ app/routes/api/clips.py
@@ -1,23 +1,45 @@
 from app import app
 
-from flask import jsonify, request
+from flask import jsonify, request, render_template
 
 
-@app.route('/api/clips/batch', methods=['GET'])
+@app.route("/api/clips/batch", methods=["GET"])
 def batch_get_clips():
     """
     Возвращает батч клипов.
     """
 
     clips_count: int = request.args.get(
-        'count', default=app.config.get('DEFAULT_CLIPS_BATCH_SIZE', 1), type=int)
+        "count", default=app.config.get("DEFAULT_CLIPS_BATCH_SIZE", 1), type=int
+    )
 
     MOCK_CLIPS: list[dict] = [
-        {'id': 1, 'content': '<b>Hello</b>, world!'},
-        {'id': 2, 'content': 'This is a <i>test</i> clip.'},
-        {'id': 3, 'content': 'Flask is <u>awesome</u>!'},
+        {
+            "id": 1,
+            "content": render_template(
+                "clip.html",
+                word_html='пр<span class="missing-letter"> </span>ломление',
+                answers=["е", "и"],
+            ),
+        },
+        {
+            "id": 2,
+            "content": render_template(
+                "clip.html",
+                word_html='н<span class="missing-letter"> </span>важдение',
+                answers=["а", "о"],
+            ),
+        },
+        {
+            "id": 3,
+            "content": render_template(
+                "clip.html",
+                word_html='стел<span class="missing-letter"> </span>щий',
+                answers=["ю", "я"],
+            ),
+        },
     ]
 
     # В будущем проверять xss
 
-    return jsonify({'clips': MOCK_CLIPS[:clips_count]})
+    return jsonify({"clips": MOCK_CLIPS[:clips_count]})
